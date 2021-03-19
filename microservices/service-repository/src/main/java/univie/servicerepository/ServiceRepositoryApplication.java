894ed40356ade3d33fa8d9e7bc8161ad02426c74
Jelbuldin Ruslan <a01407036@unet.univie.ac.at>
2021-03-20 00:33:22 +0100

    initial commit

---
+++ microservices/service-repository/src/main/java/univie/servicerepository/ServiceRepositoryApplication.java
@@ -0,0 +1,13 @@
+package univie.servicerepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRepositoryApplication.class, args);
+    }
+
+}
---
+++ microservices/service-repository/src/test/java/univie/servicerepository/ServiceRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package univie.servicerepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
