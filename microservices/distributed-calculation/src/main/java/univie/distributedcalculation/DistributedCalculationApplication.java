778f5845fd4a74d3c58ff0c287e1c9636b0a36ea
Jelbuldin Ruslan <a01407036@unet.univie.ac.at>
2021-03-20 19:09:21 +0100

    distributed calculation service initial commit

---
+++ microservices/distributed-calculation/src/main/java/univie/distributedcalculation/DistributedCalculationApplication.java
@@ -0,0 +1,32 @@
+package univie.distributedcalculation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+import univie.distributedcalculation.controller.Controller;
+import univie.distributedcalculation.model.MicroserviceInfo;
+
+import javax.annotation.PostConstruct;
+import java.security.Provider;
+
+@SpringBootApplication
+public class DistributedCalculationApplication {
+
+    private Controller controller;
+
+    @Value("${server.port}")
+    private int port;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedCalculationApplication.class, args);
+    }
+
+    @PostConstruct
+    void postConstruct(){
+        new RestTemplate().postForObject("http://localhost:7000/register", new MicroserviceInfo(
+                "localhost", port, "Addition of two numbers.", "add"), String.class);
+    }
+
+}
---
+++ microservices/distributed-calculation/src/main/java/univie/distributedcalculation/controller/Controller.java
@@ -0,0 +1,14 @@
+package univie.distributedcalculation.controller;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Controller {
+
+    private void registerMe() {
+
+    }
+
+
+
+}
